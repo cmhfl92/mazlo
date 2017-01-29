c1428cb1b5e439e157ab95da45d2676e899d018b
Christa Hegedus <Christa.Hegedus1@gmail.com>
2017-01-28 23:22:41 -0500

    Added sections to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,24 @@ class Home extends Component {
   render () {
     return <div>
       <header className='home'><h1>Mazlo</h1></header>
-      <p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.</p>
-      <nav className='home'>
-        <Link to='/TopPicks'>Become Our Guest</Link>
+      <p className='values'>
+        <h2>Our Values</h2>
+        Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.
+      </p>
+      <p className='guest'>
+        <h2>Become Our Guest</h2>
+        Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.
+      <nav className='guest'>
+        <Link className='guest' to='/TopPicks'>Become Our Guest</Link>
       </nav>
+      </p>
+      <p className='chef'>
+        <h2>Become a Chef</h2>
+        Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.
+      <nav className='chef'>
+        <Link className='chef' to='/TopPicks'>Become A Chef</Link>
+      </nav>
+      </p>
     </div>
   }
 }

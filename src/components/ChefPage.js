81e426c75f493bc7469167b33d942310cd162142
Christa Hegedus <Christa.Hegedus1@gmail.com>
2017-01-30 13:24:18 -0500

    potential updates

--- src/components/ChefPage.js
+++ src/components/ChefPage.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react'
 
 class ChefPage extends Component {
 
+  // Pick a Day and Time-react input calendar
+  // Choose A Meal
+  // Choose a Price
+  // Choose a Description
+  // Choose Ingredients
+  // Choose Address
+  // Choose Total Number of Guests Allowed
+
   render () {
     return <div />
   }

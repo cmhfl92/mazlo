93c7ec8615f1185060d3aece9aed537e8fa337a4
Christa Hegedus <Christa.Hegedus1@gmail.com>
2017-02-02 11:15:53 -0500

    forms added to chefpage

--- src/components/ChefPage.js
+++ src/components/ChefPage.js
@@ -11,10 +11,40 @@ class ChefPage extends Component {
   // Choose Total Number of Guests Allowed
 
   render () {
+    // <input type='number' min={1} max={8} value={this.state.guestCount} onChange={(e) => this.setState({guestCount: e.target.value})} />
     return <div className='chefcanvas'>
-      <div className='chefhaze' />
+      {/* <div className='chefhaze' /> */}
       <li className='chefpage'><Link to='/'><img className='chefpage' src='https://github.com/cmhfl92/mazlo/blob/master/24white.png?raw=true' /></Link></li>
       <h1> Create a Sense of Community</h1>
+      <form>
+        <h3> Step 1 </h3>
+        <p>Set a Date and Time</p>
+        <p>
+          <h3> Step 2 </h3>
+          <label htmlFor='fullname'>Name of Dish</label>
+          <input name='dishname' type='text' />
+        </p>
+        <p>
+          <h3> Step 3 </h3>
+          <label htmlFor='price'>Price per Dish</label>
+          <input price='number' type='number' min={1} max={10} />
+        </p>
+        <p>
+          <h3> Step 4 </h3>
+          <label htmlFor='description'>Ingredients</label>
+          <textarea cols='30' rows='5' />
+        </p>
+        <p>
+          <h3> Step 5 </h3>
+          <label htmlFor='address'>Address of Event</label>
+          <input name='address' type='adress' />
+        </p>
+        <p>
+          <h3> Step 6 </h3>
+          <label htmlFor='number'>Number of Seats Available</label>
+          <input number='seats' type='number' min={1} />
+        </p>
+      </form>
     </div>
   }
 }
